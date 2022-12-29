2ed1a11121602ba83735e0046d51b54e7eede86c
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2022-12-28 21:35:38 -0600

    footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -8,6 +8,20 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <hr />
+      <footer>
+        <p>
+          The literal and graphical information presented on this site about
+          Magic: The Gathering, including card images, mana symbols, and Oracle
+          text, is copyright Wizards of the Coast, LLC, a subsidiary of Hasbro,
+          Inc.
+        </p>
+        <p>
+          Portions of content & imagery for Magic: The Gathering is provided by
+          Scryfall API and is not associated with this product.
+        </p>
+        <p>&copy; Kyle Knight for all the other things</p>
+      </footer>
     </Html>
   );
 }
