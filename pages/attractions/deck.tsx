37fb07dd895da271e9b4e34208226a8165124438
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2023-01-12 20:30:51 -0600

    using attraction deck

--- pages/attractions/deck.tsx
+++ pages/attractions/deck.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useLayoutEffect, useCallback, useMemo, useRef, useState } from "react";
 import Navigation from "../../components/Navigation";
 import { useSupabaseContext } from "../../context/Supabase";
-import type { Attraction, Deck } from "../../utils/types";
+import type { Attraction } from "../../utils/types";
 import { shuffle } from "../../utils/deck";
 
 import { useDeckContext } from "../../context";
@@ -11,13 +11,12 @@ import Card from "../../components/Card";
 export default function AttractionsDeck() {
   const { supabase, user } = useSupabaseContext();
   const loadedAttractions = useRef(false);
-  // const currentCardIndex = useRef(-1);
   const [currentCardIndex, setCurrentCardIndex] = useState(-1);
   if (!supabase || !user) throw new Error("How did you even get here?");
   const { getDecks, loadingDecks } = useDeckContext();
-  const [deck, setDeck] = useState<Deck>();
   const [availableCards, setAvailableCards] = useState<Attraction[]>([]);
   const [ongoingCards, setOngoingCards] = useState<Attraction[]>([]);
+  const [junkyardCards, setJunkyardCards] = useState<Attraction[]>([]);
 
   const reverseOrderOngoingCards = useMemo(
     () => [...ongoingCards].reverse(),
@@ -31,28 +30,56 @@ export default function AttractionsDeck() {
 
     setCurrentCardIndex(nextIdx);
     setOngoingCards(nextOngoingCards);
-  }, [setOngoingCards, currentCardIndex, availableCards]);
+  }, [setOngoingCards, currentCardIndex, availableCards, ongoingCards]);
+
+  const handleSendToJunkyard = useCallback(
+    (card: Attraction) => {
+      if (
+        window.confirm(
+          `Are you sure you want to send '${card.name}' to the Junkyard?`
+        )
+      ) {
+        const junkIdx = ongoingCards.findIndex((c) => c.id === card.id);
+        const updatedOngoingCards = [...ongoingCards];
+        const newJunkyard = [...junkyardCards];
+        newJunkyard.push(card);
+        setJunkyardCards(newJunkyard);
+        updatedOngoingCards.splice(junkIdx, 1);
+        console.log(updatedOngoingCards);
+        setOngoingCards(updatedOngoingCards);
+      }
+    },
+    [ongoingCards, setOngoingCards]
+  );
+
+  const handleResetAttractions = useCallback(() => {
+    if (window.confirm(`Are you sure you want to reset this game?`)) {
+      setOngoingCards([]);
+      setCurrentCardIndex(-1);
+      setAvailableCards(shuffle(availableCards));
+    }
+  }, []);
 
   useLayoutEffect(() => {
-    if (!deck && !loadedAttractions.current) {
+    if (!availableCards.length && !loadedAttractions.current) {
       loadedAttractions.current = true;
       getDecks("attractions").then((getDecksResponse) => {
-        if (getDecksResponse?.length) setDeck(getDecksResponse[0]);
+        if (getDecksResponse?.length) {
+          const cards: { selected: boolean; card: Attraction }[] = JSON.parse(
+            getDecksResponse[0]?.cards || ""
+          );
+          const available = cards
+            .filter(({ selected }) => !!selected)
+            .map(({ card }) => card);
+          setAvailableCards(shuffle(available));
+        }
       });
     }
-  }, [deck, supabase, user, getDecks]);
+  }, [supabase, user, getDecks]);
 
   useLayoutEffect(() => {
-    if (deck) {
-      const cards: { selected: boolean; card: Attraction }[] = JSON.parse(
-        deck?.cards || ""
-      );
-      const available = cards
-        .filter(({ selected }) => !!selected)
-        .map(({ card }) => card);
-      setAvailableCards(shuffle(available));
-    }
-  }, [deck]);
+    console.log({ reverseOrderOngoingCards });
+  }, [reverseOrderOngoingCards]);
 
   return (
     <div id="attractions-page">
@@ -70,31 +97,24 @@ export default function AttractionsDeck() {
             <button onClick={handleVisitAttraction}>
               Visit New Attraction
             </button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button onClick={handleResetAttractions}>Reset Attractions</button>
           </p>
 
           {reverseOrderOngoingCards.map((attraction) => {
             const imagePath = attraction["image_uris/normal"];
             return (
-              <div key={attraction?.id}>
+              <div key={attraction.id}>
                 <p>
-                  <strong>{attraction?.name}</strong>
+                  <strong>{attraction.name}</strong>
                 </p>
                 <p>
                   {imagePath && (
-                    <Card src={imagePath} alt={`${attraction?.oracle_text}`} />
+                    <Card src={imagePath} alt={`${attraction.oracle_text}`} />
                   )}
                 </p>
-                <button
-                  onClick={() => {
-                    if (
-                      window.confirm(
-                        `Are you sure you want to send '${attraction?.name}' to the Junkyard?`
-                      )
-                    ) {
-                      console.log("Send to junkyard", attraction?.id);
-                    }
-                  }}>
-                  Send &apos;{attraction?.name}&apos; to Junkyard
+                <button onClick={() => handleSendToJunkyard(attraction)}>
+                  Send &apos;{attraction.name}&apos; to Junkyard
                 </button>
               </div>
             );
